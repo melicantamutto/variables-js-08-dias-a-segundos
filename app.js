913e62bef748d2c040511d4cf0a20e700f6a1425
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:57:16 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Días a segundos')
+
+alert('Días a segundos')
+
+const dias = prompt('Ingresa la cantidad de dias');
+const segundos = (dias * 24 * 60 * 60);
+
+alert(`${dias} dias son ${segundos} segundos`);
